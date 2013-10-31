9d0b5248215c20eb8907966c656088bc9cf91c2a
Jovi Nel <jnp.parilla@gmail.com>
2013-10-31 17:09:44 +0800

    Add a StaticPages controller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,14 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-#SampleApp::Application.config.secret_token = '73292f1c487ff3224b942f602c1a8523fa4149333a461168a0540c4e73bbcf08e86f6b9e870b6da06f52141392a001b5b11044825e194c42794e78a0b4a6ff41'
+SampleApp::Application.config.secret_token = '73292f1c487ff3224b942f602c1a8523fa4149333a461168a0540c4e73bbcf08e86f6b9e870b6da06f52141392a001b5b11044825e194c42794e78a0b4a6ff41'
 
+
+
+=begin
+rescue Exception => e
+	
+end
 require 'securerandom'
 
 def secure_token
@@ -21,4 +27,6 @@ def secure_token
 	end
 end
 
-SampleApp::Application.config.secret_key_base = secure_token
+SampleApp::Application.config.secret_key_base = secure_token 
+
+=end
